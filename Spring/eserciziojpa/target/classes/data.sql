329f0000abc7c2609d265e4104e316d975abd553
Enrico <enri.bellagamba@gmail.com>
2025-08-24 22:25:21 +0200

    aggiunta cartella Altro

---
+++ Spring/eserciziojpa/target/classes/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO macchina (model_name, type, color, description) VALUES
+('Panda', 'BERLINA', 'NERO', null),
+('Volkswagen', 'BERLINA', 'ROSSO', 1600),
+('Ford', 'BERLINA', 'ROSSO', 1500),
+('Toyota', 'BERLINA', 'BIANCO', 1000),
+('BMW', 'SPORTIVA', 'NERO', 2000);
---
+++ Spring/unittest/target/classes/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO user (name, surname, age) VALUES
+('Giuseppe', 'Verdi', 53),
+('Bruno', 'Rossi', 53),
+('Antonio', 'Neri', 53);
